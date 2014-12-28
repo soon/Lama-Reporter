ea732cf27367f58d3f3943ee4eae9667d48f7931
Andrew Kuchev <0coming.soon@gmail.com>
2014-12-28 14:47:49 +0500

    Added new method mark_mail_as_read

--- mail/abstract_mail_manager.py
+++ mail/abstract_mail_manager.py
@@ -7,6 +7,12 @@ __all__ = ['AbstractMailManager']
 
 
 class AbstractMailManager(object):
+    def mark_mail_as_read(self, mail):
+        self.mark_mail_as_read_by_id(mail.id)
+
+    def mark_mail_as_read_by_id(self, id):
+        raise NotImplementedError()
+
     @property
     def unread_mails(self):
         raise NotImplementedError()
