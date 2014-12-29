7b6644dca6844355802710c00d1b3816233868d1
Andrew Kuchev <0coming.soon@gmail.com>
2014-12-29 22:29:51 +0500

    Prints welcome message to console

--- main.py
+++ main.py
@@ -16,9 +16,33 @@ except ImportError:
                        'GMAIL_STORAGE'])
 
 
+def print_welcome():
+    print 'Welcome to the Lam-o-Matic, biatch'
+    print r'''
+ |                              _ \                       |
+ |      _` | __ `__ \   _` |   |   | _ \ __ \   _ \   __| __|  _ \  __|
+ |     (   | |   |   | (   |   __ <  __/ |   | (   | |    |    __/ |
+_____|\__,_|_|  _|  _|\__,_|  _| \_\___| .__/ \___/ _|   \__|\___|_|
+                                        _|
+         _ \ _ |         |       |
+\ \   / |   |  |    _` | | __ \  __ \   _` |
+ \ \ /  |   |  |   (   | | |   | | | | (   |
+  \_/  \___/_)_|  \__,_|_| .__/ _| |_|\__,_|
+                          _|
+'''
+
+
+def print_ready():
+    print 'All systems go!'
+
+
 def main():
+    print_welcome()
+
     manager = GMailManager(GMAIL_CLIENT_SECRET_JSON, storage_path=GMAIL_STORAGE)
     bot = LamaBot(VK_APP_ID, manager, chat_id=VK_CHAT_ID, login=VK_LOGIN, password=VK_PASSWORD)
+
+    print_ready()
 
     while True:
         bot.notify_about_unread_mails()
