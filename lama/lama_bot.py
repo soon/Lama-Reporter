11c995328338749263b0ef46228249e6a14b658c
Andrew Kuchev <0coming.soon@gmail.com>
2014-12-31 16:15:24 +0500

    Added sending message to dialog via private messages

--- lama/lama_bot.py
+++ lama/lama_bot.py
@@ -2,9 +2,11 @@
 """
 This module defined class for Lama Bot
 """
+from itertools import imap, ifilter
 import time
 import logging
 import vk
+from vk_message import VkMessage
 
 __all__ = ['LamaBot']
 
@@ -31,6 +33,9 @@ class LamaBot(object):
         self.password = None
         self.login = None
         self.vkapi = None
+        self.commands = {}
+
+        self.initialize_commands()
 
         self.mail_manager = mail_manager
         self.wait_for_before_check_mails = wait_for_before_check_mails
@@ -52,6 +57,11 @@ class LamaBot(object):
         else:
             raise ValueError('Either login/password or access_token are not initialized')
 
+    def initialize_commands(self):
+        self.commands = {
+            'post_to_dialog': self.post_message_to_dialog
+        }
+
     def notify_about_unread_mails(self):
         for m in self.unread_mails:
             posted, exception = self.try_post_mail(m)
@@ -60,36 +70,84 @@ class LamaBot(object):
             else:
                 logging.error(exception)
 
+    def check_private_messages(self):
+        for m in filter(lambda x: x.body is not None, self.unread_private_messages):
+            self.execute(m.body)
+            self.mark_message_as_read(m)
+
     @property
     def unread_mails(self):
         self.mail_manager.reset_connection()
         return self.mail_manager.unread_mails
 
+    @property
+    def unread_private_messages(self):
+        return filter(lambda m: m.is_unread, self.private_messages_iter)
+
+    @property
+    def private_messages_iter(self):
+        return ifilter(lambda m: m.is_private, self.messages_iter)
+
+    @property
+    def messages_iter(self):
+        return imap(VkMessage, self.raw_messages)
+
+    @property
+    def raw_messages(self):
+        self.initialize_vkapi()
+        response = self.vkapi.messages.get()
+        return response.get('items', [])
+
     def try_post_mail(self, mail):
         return self.try_post_message(self.wrap_mail(mail))
 
     def post_mail(self, mail):
-        self.post_message(self.wrap_mail(mail))
+        self.post_message_to_dialog(self.wrap_mail(mail))
 
     def try_post_message(self, message):
         try:
-            self.post_message(message)
+            self.post_message_to_dialog(message)
             return True, None
         except Exception, e:
             return False, e
 
-    def post_message(self, message):
+    def try_post_message_and_log_if_failed(self, message):
+        posted, exception = self.try_post_message(message)
+        if not posted:
+            logging.error(exception)
+        return posted
+
+    def execute(self, s):
+        command, args = self.split_to_command_and_argument(s)
+        if command in self.commands:
+            self.commands[command](args)
+        else:
+            self.command_not_found(command)
+
+    def split_to_command_and_argument(self, command):
+        values = command.split(':', 1)
+        if len(values) != 2:
+            values.append(None)
+        return values[0], values[1]
+
+    def post_message_to_dialog(self, message):
         self.initialize_vkapi()
         self.vkapi.messages.send(chat_id=self.chat_id, message=message)
 
     def post_welcome_message(self):
-        self.post_message('The Lama is ready to work! (version {0})'.format(self.version))
+        self.post_message_to_dialog('The Lama is ready to work! (version {0})'.format(self.version))
+
+    def command_not_found(self, command):
+        message = 'Command {} not found'.format(command)
+        self.try_post_message_and_log_if_failed(message)
+        logging.warning(message)
 
     def run(self):
         self.post_welcome_message()
 
         while True:
             self.notify_about_unread_mails()
+            self.check_private_messages()
             time.sleep(60)
 
     @staticmethod
@@ -98,3 +156,11 @@ class LamaBot(object):
             subject=mail.subject,
             sender=mail.sender,
             body=mail.body)
+
+    def mark_message_as_read(self, message):
+        self.mark_message_as_read_by_id(message.id)
+
+    def mark_message_as_read_by_id(self, message_ids):
+        self.vkapi.messages.markAsRead(message_ids=message_ids)
+
+
