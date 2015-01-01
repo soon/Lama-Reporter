b9c30cf100554ec5e06b5068812a2b2afa76a399
Andrew Kuchev <0coming.soon@gmail.com>
2015-01-01 13:17:22 +0500

    Added method safe_mark_mail_as_read_and_log_if_failed and property safe_unread_mails_and_log_if_failed

--- mail/abstract_mail_manager.py
+++ mail/abstract_mail_manager.py
@@ -1,6 +1,7 @@
 """
 This module defines base class for mail managers
 """
+from utils import safe_call_and_log_if_failed
 
 
 __all__ = ['AbstractMailManager']
@@ -10,9 +11,17 @@ class AbstractMailManager(object):
     def mark_mail_as_read(self, mail):
         self.mark_mail_as_read_by_id(mail.id)
 
+    @safe_call_and_log_if_failed
+    def safe_mark_mail_as_read_and_log_if_failed(self, mail):
+        self.mark_mail_as_read(mail)
+
     def mark_mail_as_read_by_id(self, id):
         raise NotImplementedError()
 
     @property
     def unread_mails(self):
+        raise NotImplementedError()
+
+    @property
+    def safe_unread_mails_and_log_if_failed(self):
         raise NotImplementedError()
